81172b4aaa24749045cd5d0a813ba3b5247a1306
Mare Salazar <mare.salazar790@gmail.com>
2026-07-05 21:36:30 -0500

    // Prototipos de funciones para la limpieza de buffer, salto linea y validacion

---
+++ utilidades.h
@@ -0,0 +1,13 @@
+#ifndef Utilidades_H
+#define Utilidades_H
+
+// Prototipos de funciones para la limpieza de buffer y quitar salto de linea
+void limpiarBuffer();
+void quitarSL (char cadena[]);
+
+//Funciones de validacion
+int validarCodigo(char codigo[]);
+int validarFecha(char fecha[]);
+int validarHora (char hora[]);
+
+#endif
